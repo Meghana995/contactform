7e380ecd0a1ed911639a345cf06f5a9956d00043
Meghana P <meghanasasidharanp@gmail.com>
2024-03-25 08:55:30 +0400

    second-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contactform';
+
+  @ViewChild('signInBtn', { static: true }) signInBtn!: ElementRef;
+  @ViewChild('signUpBtn', { static: true }) signUpBtn!: ElementRef;
+  @ViewChild('container', { static: true }) container!: ElementRef;
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    this.signUpBtn.nativeElement.addEventListener('click', () => {
+      this.container.nativeElement.classList.add('sign-up-mode');
+    });
+
+    this.signInBtn.nativeElement.addEventListener('click', () => {
+      this.container.nativeElement.classList.remove('sign-up-mode');
+    });
+  }
 }
